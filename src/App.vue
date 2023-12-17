c8c7b00155e0c0eda0be3a65bc70f463b76cc759
Ivan Degtyarev <degtyareviv080@gmail.com>
2023-12-17 19:56:59 +0200

    Base

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
 import Header from './components/Header.vue'
+import GameCard from './components/GameCard.vue'
+import { RouterView } from 'vue-router';
 </script>
 
 <template>
 
- <Header />
+ 
+ <div class="app">
+    <GameCard/>
+    <RouterView></RouterView>
+ </div>
 </template>
 
 <style scoped>
-
+    .app{
+        width: 100%;
+        min-height: 100vh;
+        background-color: #1f1f1f;
+    }
 </style>
